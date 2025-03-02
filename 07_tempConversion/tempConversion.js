8bcfcf18f50ded9ca1df569eb4e5ae3c0cacf87e
Alvin Karlsson <alvin.karlsson1234@gmail.com>
2025-03-02 17:20:23 +0100

    Completed 01-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  let num = (x - 32) * (5 / 9);
+  return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (x) {
+  let num = x * (9 / 5) + 32;
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
